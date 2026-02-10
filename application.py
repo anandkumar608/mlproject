cbcdad6e17de472d56f2db51a735335b263f5b30
anandkumar608 <monibatu10@gmail.com>
2026-02-10 13:28:43 +0530

    application.py

--- application.py
+++ application.py
@@ -16,9 +16,9 @@ def index():
     return render_template('index.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
-def index():
+def predict_datapoint():
     if request.method=='GET':
-        return render_template('index.html')
+        return render_template('home.html')
     else:
         data=CustomData(
             gender=request.form.get('gender'),
@@ -26,8 +26,8 @@ def index():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -38,10 +38,10 @@ def index():
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
-        return render_template('index.html',results=results[0])
+        return render_template('home.html',results=results[0])
     
 
-# if __name__=="__main__":
-#     app.run()        
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
 
 
